552fb27d32d34dc87ba521fa31cf09b7c16d82a0
kerry opiyo <kerryopiyo0758@gmail.com>
2026-06-09 16:23:06 +0300

    storage

--- utils/storage.py
+++ utils/storage.py
@@ -3,8 +3,11 @@ import json
 FILE_PATH = 'data/data.json'
 
 def load_data():
-    with open(FILE_PATH, 'r') as f:
-        return json.load(f)
+    try:
+        with open(FILE_PATH, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {"users": []}  
 
 def save_data(data):
     with open(FILE_PATH, 'w') as f:
