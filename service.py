ebd71741e2ebded384b0d669ec420498fc79c7be
kerry opiyo <kerryopiyo0758@gmail.com>
2026-06-11 15:32:44 +0300

    Aadded

--- service.py
+++ service.py
@@ -1,18 +1,1 @@
-from models.user import user
-from models.task import task
-from models.project import project
-from utils.storage import storage
 
-class TaskManagerService:
-    def__init__(self):
-     self.data = load_data()
-    
-    def add_user(self, user_id, name):
-       self.data['users'].append({
-            "user_id": user_id,
-            "name": name,
-            "projects": []
-        })
-        save_data(self.data)
-       
-       
