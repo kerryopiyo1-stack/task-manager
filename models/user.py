efd778530fa312691bc648cc1a5a8d18cbe80a05
kerry opiyo <kerryopiyo0758@gmail.com>
2026-06-11 15:48:22 +0300

    added

--- models/user.py
+++ models/user.py
@@ -1,7 +1,11 @@
 class User:
-    def __init__(self, user_id, name):
-        self.user_id = user_id
-        self.name = name
+    def __init__(self, user_id_or_name, name=None):
+        if name is None:
+            self.user_id = None
+            self.name = user_id_or_name
+        else:
+            self.user_id = user_id_or_name
+            self.name = name
         self.projects = []
 
     def add_project(self, project):
---
+++ tests/test_user.py
@@ -0,0 +1,6 @@
+from models.user import User
+
+user = User("Kerry")
+
+print(user.name)
+print(user.projects)
